251765abeede5a481a6d1a66dfeb2187622144e4
MehfoozAbbas110 <33392056+MehfoozAbbas110@users.noreply.github.com>
2024-06-23 21:55:35 +0530

    update

---
+++ 03_JS_Conversion_Operations.js
@@ -0,0 +1,37 @@
+// *****************************Conversion************************************
+
+let score = "null"
+// console.log(score);
+// console.log(typeof score);
+
+let newScore = Number(score)
+// console.log(newScore);
+// console.log(typeof newScore);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+// 1 => true , 0 => flase
+// "" => flase
+//"Mehfooz" => ture
+
+
+
+
+// **************************OPERATIONS*******************************************
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}`);
+console.log(`y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
